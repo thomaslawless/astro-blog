5478079cd057d259a2e3d6faf7662dc23473fc7b
Thomas Lawless <lawlessptom@gmail.com>
2023-10-10 11:55:36 -0400

    hamburger in nav on mobile

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import avatar from "../public/avatar.png?url"
+import avatar from "../../public/avatar.png?url"
 import { motion } from 'framer-motion'
 import { useMediaQuery } from "../util/useMediaQuery"
 
@@ -29,19 +29,36 @@ export default function Nav(){
             </div>
 
             {/* Title */}
-
             <h1 className="text-lg font-bold">
                 <a href="/">Hua.</a>
             </h1>
-
+{/* check if on mobile or not */}
+            {matches && (
+            <div className="flex gap-12">
+                <a href="/">Home</a>
+                <a href="/services">Services</a>
+                <a href="/contact">Contact</a>
+            </div>
+            )}
+            {!matches && (
             <div 
                 onClick={() => setToggled(prevToggle => !prevToggle)}
-                className="space-y-1 cursor-pointer xl: hidden"
+                className="space-y-1 cursor-pointer z-50"
              >
                 <span className="block h-0.5 w-8 bg-black"></span>
                 <span className="block h-0.5 w-6 bg-black"></span>
                 <span className="block h-0.5 w-4 bg-black"></span>
             </div>
+            )}
+            {toggled && matches && (
+            <div className="fixed flex bg-white bottom-0 left-0 w-full h-screen items-center justify-center">
+                <div className="flex flex-col gap-24 text-large">
+                <a href="/">Home</a>
+                <a href="/services">Services</a>
+                <a href="/contact">Contact</a>
+                </div>
+            </div>
+            )}
         </nav>
     )
 }
